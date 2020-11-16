436d19a500e4a876489f58cb1e7a860515b66636
Eric Folenta <ericfolenta@gmail.com>
2020-11-15 18:28:03 -0800

    removed footer component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import './App.css';
 import Header from './components/Header';
-import Footer from './components/Footer';
 import { Col, Container, Row } from './components/Grid';
 import UserTable from './components/UserTable';
 import users from "./users.json";
@@ -33,7 +32,6 @@ class App extends Component {
             </Col>
           </Row>
         </Container>
-        <Footer />
       </>
     );
   }
