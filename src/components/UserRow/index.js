641d3fe9f689f0822a9289dd0cb4b9f8b30b5c1b
Eric Folenta <ericfolenta@gmail.com>
2020-11-15 18:15:24 -0800

    added rows

---
+++ src/components/UserRow/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function UserRow(props) {
+  return (
+    <>
+        <tr>
+            <th scope="row">{props.id}</th>
+            <td>{props.name}</td>
+            <td>{props.department}</td>
+            <td>{props.position}</td>
+        </tr>
+    </>
+  );
+}
+
+export default UserRow;
