b76e59e749171eee0bfb0fa17172f78f90a5947f
Eric Folenta <ericfolenta@gmail.com>
2020-11-15 18:16:07 -0800

    updated app with new components and state for users

--- src/App.js
+++ src/App.js
@@ -1,15 +1,42 @@
-import logo from './logo.svg';
+import React, { Component } from "react";
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import { Col, Container, Row } from './components/Grid';
+import UserTable from './components/UserTable';
+import users from "./users.json";
+import UserRow from './components/UserRow';
 
-function App() {
-  return (
-    <>
-      <Header />
-      <Footer />
-    </>
-  );
+class App extends Component {
+  state = {
+    users
+  };
+
+  render() {
+    return (
+      <>
+        <Header />
+        <Container>
+          <Row>
+            <Col size="12">
+              <UserTable>
+              {this.state.users.map(user => (
+                <UserRow 
+                  id={user.id}
+                  name={user.name}
+                  department={user.department}
+                  position={user.position}
+                />
+              ))}
+              </UserTable>
+
+            </Col>
+          </Row>
+        </Container>
+        <Footer />
+      </>
+    );
+  }
 }
 
 export default App;
