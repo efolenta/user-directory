d0e84bc3c0beb37fd31562fbc6e8b6e848fe2657
Eric Folenta <ericfolenta@gmail.com>
2020-11-16 18:05:42 -0800

    commented out secondary method

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Header from './components/Header';
 import { Col, Container, Row } from './components/Grid';
 import UserTable from './components/UserTable';
 import users from "./users.json";
-import UserRow from './components/UserRow';
+// import UserRow from './components/UserRow';
 
 class App extends Component {
   state = {
@@ -18,16 +18,15 @@ class App extends Component {
         <Container>
           <Row>
             <Col size="12">
-              <UserTable>
-              {this.state.users.map(user => (
+              <UserTable />
+              {/* {this.state.users.map(user => (
                 <UserRow 
                   id={user.id}
                   name={user.name}
                   department={user.department}
                   position={user.position}
                 />
-              ))}
-              </UserTable>
+              ))} */}
             </Col>
           </Row>
         </Container>
